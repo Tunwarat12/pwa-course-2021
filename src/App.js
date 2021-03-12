5ac1b24da771681badfbdbe34949210d37ceea69
Tunwarat12 <tunwarat.kaew@bumail.net>
2021-03-13 03:22:16 +0700

    ใช้งาน layout compomnet

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-import './Chatbox';
-import {useState} from 'react';
+import './App.css';
 import { Link } from 'react-router-dom';
+import Chatbox from'./Chatbox';
 
 
 function App() {
-  const [text, setText] = useState("");
-  const [lines, setLines] = useState([]);
-  
-
-  const onTextChange = (event) => {
-    setText(event.target.value);
-  };
-
-  const onSend = () =>{
-    setLines([...lines , text]);
-
-    setText("");
-  };
-
-  const keyPress = (event) => {
-
-    console.log("hello");
-    if (event.which === 13){
-      onSend();
-    }
-  };
-
   return (
     <div className="App">
       <div className="App-header">
@@ -35,8 +13,8 @@ function App() {
           </button>
           </Link>
         </div>
-    <div className="App-chatroom">
-
+    <div className="App-content">
+       <Chatbox />
      </div>
     </div>
   );
--- src/Chatbox.js
+++ src/Chatbox.js
@@ -27,19 +27,25 @@ function Chatbox() {
   };
 
   return (
-    <div className="App">
-      <div className="App-header">
-          <Link to="/friend-list"> 
-          <button>
-            FriendList
-          </button>
-          </Link>
-        </div>
-    <div className="App-chatroom">
-
-     </div>
-    </div>
+   <div className="App col-6">
+       <div className="App-chatroom">
+           {
+               lines.map(x => {
+                   return <div className="App-chatroom-text">
+                       {x}
+                       </div>
+               })
+           }
+       </div>
+   <div className="App-textbox">
+       <input type="text" className="App-textbox-input"
+       value={text} onChange={onTextChange}onKeyPress={keyPress}/>
+    
+    <div className="App-textbox-send" onClick={onSend}>Send!</div>
+   </div>
+   </div>
   );
-}
+
+  }
 
 export default Chatbox;
